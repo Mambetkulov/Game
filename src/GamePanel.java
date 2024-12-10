148ad7e019cb6a00e3d9914928683c2af8b7b400
Mambetkulov <muganm320@gmail.com>
2024-12-10 17:07:13 +0600

    Initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    public final int maxScreenCol = 16;
+    public  final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+     int fps = 60;
+
+     TileManager tileM = new TileManager(this);
+    Keyb key = new Keyb();
+    Thread threadGame;
+    Player player = new Player(this,key);
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(key);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread ( ){
+        threadGame = new Thread(this);
+        threadGame.start();
+    }
+
+    @Override
+   public void run() {
+
+        double interval = 1000000000/fps;
+        double nextdrawTime = System.nanoTime() + interval;
+
+        while(threadGame != null){
+
+
+
+          update();
+
+          repaint();
+
+
+            try {
+                double remainTime = nextdrawTime - System.nanoTime();
+                remainTime = remainTime/1000000;
+                if(remainTime<0){
+                    remainTime = 0;
+                }
+                Thread.sleep((long) remainTime);
+                nextdrawTime += interval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+    public void update (){
+      player.update();
+    }
+
+    public void paintComponent(Graphics g){
+       super.paintComponent(g);
+
+       Graphics2D g2 = (Graphics2D)g;
+
+       tileM.draw(g2);
+
+       player.draw(g2);
+
+       g2.dispose();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+   static JFrame window;
+    public static void main(String[] args) {
+
+       window = new JFrame();
+       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       window.setResizable(false);
+       window.setTitle("First Game");
+       GamePanel gamePanel = new GamePanel();
+       window.add(gamePanel);
+       window.pack();
+        window.setLocationRelativeTo(null);
+       window.setVisible(true);
+
+       gamePanel.startGameThread();
+
+    }
+}
