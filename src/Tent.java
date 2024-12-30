6e498aaaa4618f35d6cbad74e62c737b5ee60a72
Bkarybaev <baielkarybaev@gmail.com>
2024-12-30 13:16:44 +0600

    add objek

--- src/Tent.java
+++ src/Tent.java
@@ -1,12 +1,13 @@
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Tent extends SuperObject{
 
     public Tent(){
         name = "tent";
         try{
-            image = ImageIO.read(getClass().getResourceAsStream("/objres/tent.png"));
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objres/tent.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
