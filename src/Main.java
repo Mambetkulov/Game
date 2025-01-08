6816593f73a194a2f1fcd706c6537e95afafa690
Bkarybaev <baielkarybaev@gmail.com>
2025-01-08 20:45:05 +0600

    update

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
    static JFrame window;
     public static void main(String[] args) {
 
+
+
        window = new JFrame();
+
        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
        window.setResizable(false);
        window.setTitle("First Game");
        GamePanel gamePanel = new GamePanel();
