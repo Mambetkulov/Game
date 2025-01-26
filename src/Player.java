3321e472eb0f7c15aed409f4a5cb5d9235a4763f
Bkarybaev <baielkarybaev@gmail.com>
2025-01-26 10:58:49 +0600

    update

--- src/Player.java
+++ src/Player.java
@@ -30,11 +30,12 @@ public class Player extends Entity{
     public void setDefaultValues(){
         worldX = gp.tileSize * 23;
         worldY = gp.tileSize * 21;
-        speed = 5;
+        speed = 4;
         direction = "up";
         // player status
 
         maxLife = 6;
+
         life = maxLife;
 
     }
