6460291e0e3d85b68cc91996211417b91c7879a8
Bkarybaev <baielkarybaev@gmail.com>
2025-01-28 14:16:20 +0600

    last righ

--- src/Player.java
+++ src/Player.java
@@ -31,6 +31,8 @@ public class Player extends Entity{
         worldX = gp.tileSize * 23;
         worldY = gp.tileSize * 21;
         speed = 4;
+
+
         direction = "up";
         // player status
 
