aa75de6c64dfa3cb7a108dc63fa6b367b6ba6d4c
Bkarybaev <baielkarybaev@gmail.com>
2025-02-11 16:06:20 +0600

    last name delete

--- src/Tent.java
+++ src/Tent.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 public class Tent extends SuperObject{
 
     public Tent(GamePanel gp){
+
         name = "tent";
         try{
-
             image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objres/tent.png")));
             uTool.scaleImage(image,gp.tileSize,gp.tileSize);
         } catch (IOException e) {
