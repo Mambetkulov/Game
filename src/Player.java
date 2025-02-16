d0e9c221af99c966d344241e20cc5931931bbfa8
Bkarybaev <baielkarybaev@gmail.com>
2025-02-16 19:49:00 +0600

    as

--- src/Player.java
+++ src/Player.java
@@ -101,7 +101,6 @@ public class Player extends Entity {
             String objectName = gp.obj[index].name;
 
             switch (objectName) {
-
                 case "key": {
                     gp.playSe(1);
                     haskey++;
