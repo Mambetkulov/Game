82ea9e8cfed1e87a853d5dddc85a0c34d452da96
Mambetkulov <muganm320@gmail.com>
2025-03-02 09:15:21 +0600

    dialogue

--- src/Player.java
+++ src/Player.java
@@ -136,8 +136,12 @@ public class Player extends Entity{
 
     public void  interactNPC(int i){
         if(i != 999){
-            System.out.println("you are hitting a  npc!");
+            if(gp.key.enterPressed == true){
+                gp.gameState = gp.dialogueState;
+                gp.npc[i].speak();
+            }
         }
+        gp.key.enterPressed = false;
     }
 
 
