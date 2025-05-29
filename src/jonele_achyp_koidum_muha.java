6652ce1b3a07b722bb2a1a75f7148e57d0008a69
Bkarybaev <baielkarybaev@gmail.com>
2025-05-29 19:00:46 +0600

    add pole

--- src/jonele_achyp_koidum_muha.java
+++ src/jonele_achyp_koidum_muha.java
@@ -4,6 +4,7 @@ public class jonele_achyp_koidum_muha {
     private String jon ;
     private int age;
     private boolean a = true;
+    private int bosh = 2;
     public jonele_achyp_koidum_muha() {
     }
     public jonele_achyp_koidum_muha(String jon, int age, boolean a) {
@@ -42,12 +43,10 @@ public class jonele_achyp_koidum_muha {
         jonele_achyp_koidum_muha that = (jonele_achyp_koidum_muha) o;
         return age == that.age && a == that.a && Objects.equals(jon, that.jon);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(jon, age, a);
     }
-
     @Override
     public String toString() {
         return "jonele_achyp_koidum_muha{" +
