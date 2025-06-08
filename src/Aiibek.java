ab11df389d6335edb01b0b806e0589d6ca1e8748
Aibek <clesh.meder.271@gmail.com>
2025-06-09 00:10:00 +0600

    Ad a

--- src/Aiibek.java
+++ src/Aiibek.java
@@ -126,7 +126,8 @@ public class Aiibek {
      System.out.println("BAIEL");
      System.out.println("BAIEL");
      System.out.println("BAIEL");
-     System.out.println("BAIEL");g
+     System.out.println("BAIEL");
+     System.out.println("BAIEL");
 
 
 
